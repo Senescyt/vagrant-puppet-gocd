96efd662063e21550e3f441929481801491ac46c
Marcelo Oliveira <Marcelo@Marcelos-MacBook-Pro.local>
2014-06-16 15:10:12 -0500

    Arreglando el archivo pp del  goserver para utilizar el archivo gobase

--- modules/goserver/manifests/init.pp
+++ modules/goserver/manifests/init.pp
@@ -2,30 +2,34 @@
 #
 #
 class goserver {
+  require gobase
+
   package { 'java-1.7.0-openjdk.x86_64':
     ensure => installed
   }
-  
-  exec { 'download-goserver':
-    command => "/usr/bin/wget http://download01.thoughtworks.com/go/14.1.0/ga/go-server-14.1.0-18882.noarch.rpm -O /tmp/goserver.rpm",
-    creates => "/tmp/goserver.rpm"
+
+  package {
+    "go-server":
+      ensure  => installed,
+      require => Yumrepo['ThoughtWorks']
   }
-  
-  exec { 'install-goserver':
-    command => "/bin/rpm -i /tmp/goserver.rpm",
-    creates => "/var/lib/go-server",
-    require => [Package['java-1.7.0-openjdk.x86_64'], Exec['download-goserver']]
+
+  file{
+    '/etc/default/go-server':
+      ensure  => present,
+      require => Package['go-server'],
+      content => template('goserver/go-server.erb')
   }
-  
+
+  service { 'go-server' :
+    ensure  => running,
+    enable  => true,
+  }  
+
   firewall { '000 allow 8153 and 8154':
     port   => [8153, 8154],
     proto  => tcp,
     action => accept,
   }
   
-  service { "go-server":
-    ensure     => running,
-    enable     => true,
-    require    => Exec['install-goserver']
-  }
 }
